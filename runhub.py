b717006a85c870e2ec7b2781c11794d7f43def76
wochanda <wochanda@gmail.com>
2014-02-01 10:29:26 -0800

    bugfix

--- runhub.py
+++ runhub.py
@@ -1,22 +1,22 @@
 """
 Launcher for homity-hub
 """
-from Hub import APP
-from Hub.api import HUB_CONFIG
-if HUB_CONFIG.get('ssl_enable'):
+from Hub import app
+from Hub.api import hub_config
+if hub_config.get('ssl_enable'):
     from OpenSSL import SSL
 
 def main():
     """ Main. """
-    if HUB_CONFIG.get('ssl_enable'):
+    if hub_config.get('ssl_enable'):
         context = SSL.Context(SSL.SSLv23_METHOD)
-        context.use_privatekey_file(HUB_CONFIG.get('ssl_private_key'))
-        context.use_certificate_file(HUB_CONFIG.get('ssl_cert'))
-        APP.run(host='0.0.0.0',
+        context.use_privatekey_file(hub_config.get('ssl_private_key'))
+        context.use_certificate_file(hub_config.get('ssl_cert'))
+        app.run(host='0.0.0.0',
                 ssl_context=context,
                 debug=False)
     else:
-        APP.run(host='0.0.0.0', debug=False)
+        app.run(host='0.0.0.0', debug=False)
 
 if __name__ == '__main__':
     main()
