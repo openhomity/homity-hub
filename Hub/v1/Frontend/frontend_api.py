2d9258e7c8fe7be274969d0aad2b69d1e4081700
wochanda <wochanda@gmail.com>
2014-11-30 14:42:02 -0800

    f

--- Hub/v1/Frontend/frontend_api.py
+++ Hub/v1/Frontend/frontend_api.py
@@ -1,16 +1,15 @@
 """
 TBD
 """
-from Hub import app
 from flask import Blueprint, request, make_response
 import json
 
 from Hub.v1.Common.auth import requires_auth
 
 
-V1SPOKE = Blueprint('V1FRONTEND', __name__)
+V1FRONTEND = Blueprint('V1FRONTEND', __name__)
 
-@app.route('/', methods=['GET'])
+@V1FRONTEND.route('/', methods=['GET'])
 @requires_auth
 def get_all_status():
     """Placeholder for root get."""
